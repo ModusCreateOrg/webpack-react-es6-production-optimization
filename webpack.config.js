9395f2ee55bcbf4ec0de32440df31a94c5254725
Grgur Grisogono <grgur@grgur.com>
2015-11-25 13:01:37 +0100

    Step by step optimization

--- webpack.config.js
+++ webpack.config.js
@@ -3,25 +3,32 @@ const webpack = require('webpack');
 
 module.exports = {
   devtool: 'eval',
+
+  // Step 1: Source Maps
+  // devtool: 'cheap-module-source-map',
+
   entry: './index',
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
     publicPath: '/static/',
   },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': '"production"'
-      }
-    })
-  ],
+
+  // Step 2: Node environment
+  // plugins: [
+  //   new webpack.DefinePlugin({
+  //     'process.env': {
+  //       'NODE_ENV': JSON.stringify('production')
+  //     }
+  //   })
+  // ],
+
   module: {
     loaders: [
       {
         test: /\.js$/,
-        loaders: ['uglify', 'babel'],
+        loaders: ['babel']
       }
-    ],
-  },
+    ]
+  }
 };
